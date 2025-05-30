0af2b5055a9e13cfb3b4f8fda9f657d5ac2da4da
LeLekambageek <capidelamordicus@gmail.com>
2025-05-30 11:57:50 +0000

    creations du dossier layouts

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,5 @@ Route::resource('etudiant', EtudiantController::class);
 Route::resource('evaluation', EvaluationController::class);
 
 //routes supplementaires pour les notes
-Route::get('/evaluations/grade',[EvaluationController::class, 'grade'])->name('evaluation.grade');
-Route::post('/evaluations/update-grades', [EvaluationController::class, 'updateGrades'])->name('evaluations.update-grades');
+Route::get('/evaluations/{evaluation}/grade',[EvaluationController::class, 'grade'])->name('evaluation.grade');
+Route::post('/evaluations/{evaluation}/update-grades', [EvaluationController::class, 'updateGrades'])->name('evaluations.update-grades');
